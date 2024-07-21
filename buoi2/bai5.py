fc654f32ee4e68b41d96fc37edfc2346607067e9
TuanAnh0002 <ta060925@gmail.com>
2024-07-21 12:31:04 +0700

    btvn-b2

--- buoi2/bai5.py
+++ buoi2/bai5.py
@@ -1,15 +1,11 @@
+from math import *
 print("Nhập vào số n")
 a=int(input())
-b = 0
-c = 0
-for i in range (1,a+1) :
-	while (a > 0):
-		b = b + a % 10
-		c = c + b ** 3
-		a = int(a / 10)
-if c == a :
-	print()
-else :
-	print()
-
+for i in range(1,a+1) :
+    s=0
+    b=i
+    while b>0 :
+        s+=pow((b%10),3)
+        b=b//10
+    if i==s : print(i)
 
---
+++ buoi2/bai6.py
@@ -0,0 +1,9 @@
+print("Nhap n:")
+n = int(input())
+for i in range(1,n+1) :
+    S=0
+    for j in range(1,i) :
+        if (i % j == 0) : 
+            S+=j
+    if i==S :
+        print(i,end=" ")
